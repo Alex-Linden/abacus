f65da8620140af881afdda948e58ed6f6dd27f8c
Alex Linden <alextl2004@gmail.com>
2025-01-17 13:43:19 -0800

    remove default react import

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const Button = ({ text, buttonFunc }: { text: string, buttonFunc: Function }) => {
     // const handleClick = () => {
     //     buttonFunc(text)
